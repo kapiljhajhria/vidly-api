d15602816127945726fc5af75d84ce518c40269b
kapiljhajhria <jhajhria44@gmail.com>
2021-01-05 21:12:42 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
 const Joi = require("joi");
 const genres = require("./routes/genres");
 const express = require("express");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongoDb:vidly");
+  })
+  .catch((error) =>
+    console.log("got error connecting to database vidly: " + error)
+  );
 
 app.use(express.json());
 app.use("/api/genres", genres);

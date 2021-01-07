131ac8e18215bdc0e527bfa7b4e1173841741b83
kapiljhajhria <jhajhria44@gmail.com>
2021-01-07 19:36:09 +0530

    rental api added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Joi = require("joi");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const express = require("express");
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

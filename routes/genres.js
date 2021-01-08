e4c1cfef971d5fd13bdfacbda61c25c318ab77db
kapiljhajhria <jhajhria44@gmail.com>
2021-01-08 22:20:15 +0530

    auth middleware added to all routes where data is being created or changed

--- routes/genres.js
+++ routes/genres.js
@@ -19,7 +19,7 @@ router.post("/", auth, async (req, res) => {
   res.send(genre);
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -37,7 +37,7 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre)

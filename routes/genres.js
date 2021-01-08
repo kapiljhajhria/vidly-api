a1b74e8c514f06ba4b1e0fd2b48589ad4554cca6
kapiljhajhria <jhajhria44@gmail.com>
2021-01-08 23:28:21 +0530

    added asyncMIddleware to replace try catch

--- routes/genres.js
+++ routes/genres.js
@@ -1,18 +1,18 @@
 const { Genre, validate } = require("../models/genre");
+const asyncMiddleware = require("../middleware/async");
 const mongoose = require("mongoose");
 const express = require("express");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  asyncMiddleware(async (req, res) => {
     const genres = await Genre.find().sort("name");
     res.send(genres);
-  } catch (err) {
-    next(err);
-  }
-});
+  })
+);
 
 router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);

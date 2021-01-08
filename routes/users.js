2447b70df7bcb949d2af049cd3b5749132d4ba49
kapiljhajhria <jhajhria44@gmail.com>
2021-01-08 22:20:23 +0530

    typo

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //cehck if user is registered
+  //check if user is registered
 
   let user = await User.findOne({ email: req.body.email });
 

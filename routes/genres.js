30635c74986cf29a5ee13dd9e8ba08fb08a91afa
kapiljhajhria <jhajhria44@gmail.com>
2021-01-08 22:18:26 +0530

    middle ware added to post genre api

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,7 @@
 const { Genre, validate } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
   res.send(genres);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 

945a29a65c1da7cbd80003fc8bc6c6e317fe4cf1
kapiljhajhria <jhajhria44@gmail.com>
2021-01-09 15:21:46 +0530

    JOi related code refaactored

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 
 const app = express();
@@ -7,6 +5,6 @@ require("./startup/logging")(); //logging errors
 require("./startup/routes")(app); //load the routes
 require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
-
+require("./startup/validation")(); //Joi related statements req for api endpoint events validation
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

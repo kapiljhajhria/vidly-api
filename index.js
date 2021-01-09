a44806828857b188d59ca9cd07d3f1d989ba8a77
kapiljhajhria <jhajhria44@gmail.com>
2021-01-09 15:18:43 +0530

    config related code refactored

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const config = require("config");
-
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
@@ -8,11 +6,7 @@ const app = express();
 require("./startup/logging")(); //logging errors
 require("./startup/routes")(app); //load the routes
 require("./startup/db")(); //connect to database
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL jwtPrivateKey is not defined");
-  process.exit(1);
-}
+require("./startup/config")(); //check for config values or env values. if not found throw error
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

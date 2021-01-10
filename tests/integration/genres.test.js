efc4264802ff11492e8ab10c0964bf0a5707a4fa
kapiljhajhria <jhajhria44@gmail.com>
2021-01-10 21:37:08 +0530

    genre GET more tests added

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,18 +1,32 @@
 let server;
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
+
 describe("/api/genres", () => {
   //call this before each test
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove();
   });
 
   describe("GET/", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+        { name: "genre3" },
+        { name: "genre4" },
+      ]);
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(4);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre3")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre4")).toBeTruthy();
     });
   });
 });

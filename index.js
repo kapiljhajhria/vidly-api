ccaf7c9dbb66e3fbcba653f972250a9707e17d20
kapiljhajhria <jhajhria44@gmail.com>
2021-01-10 21:21:31 +0530

    exporting server for tests

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
 require("./startup/validation")(); //Joi related statements req for api endpoint events validation
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;

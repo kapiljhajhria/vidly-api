818633c96e1b846e743a9c7f2a031ffc225dccb3
kapiljhajhria <jhajhria44@gmail.com>
2021-01-10 21:19:16 +0530

    temp removed mongodb logging

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require("winston");
-require("winston-mongodb");
+// require("winston-mongodb");
 require("express-async-errors");
 
 module.exports = function () {
@@ -16,10 +16,11 @@ module.exports = function () {
   });
 
   winston.add(new winston.transports.File({ filename: "logfile.log" }));
-  winston.add(
-    new winston.transports.MongoDB({
-      db: "mongodb://localhost/vidly",
-      level: "error",
-    })
-  );
+  winston.add(new winston.transports.Console());
+  // winston.add(
+  //   new winston.transports.MongoDB({
+  //     db: "mongodb://localhost/vidly",
+  //     level: "error",
+  //   })
+  // );
 };

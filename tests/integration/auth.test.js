a4f254f0169513c738334cfa6a5dddfe588e3740
kapiljhajhria <jhajhria44@gmail.com>
2021-01-11 00:02:08 +0530

    added auth middleware test

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,36 @@
+const request = require("supertest");
+const { Genre } = require("../../models/genre");
+const { User } = require("../../models/user");
+let server;
+describe("auth middleware", () => {
+  beforeEach(() => {
+    server = require("../../index");
+  });
+  afterEach(async () => {
+    await Genre.remove({});
+    server.close();
+  });
+  let token;
+  const exec = () =>
+    request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+
+  beforeEach(() => (token = new User().generateAuthToken()));
+
+  it("should return 401 if no token provided", async () => {
+    token = "";
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
+  it("should return 200 if  token provided", async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+  });
+  it("should return 400 if token is invalid", async () => {
+    token = "a";
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+});

822228474c2f6c1212bb93222ce44f8216b0fa15
kapiljhajhria <jhajhria44@gmail.com>
2021-01-10 21:19:49 +0530

    using env variable to connect to db

--- startup/db.js
+++ startup/db.js
@@ -1,15 +1,18 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
+
 mongoose.set("useFindAndModify", false);
 
 module.exports = function () {
+  const db = config.get("db");
   mongoose
-    .connect("mongodb://localhost/vidly", {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => {
-      winston.info("Connected to mongoDb:vidly");
+      winston.info(`Connected to mongoDb: ${db}`);
     })
     .catch((error) => {});
 };

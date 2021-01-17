fd8894a9f77e4782f3de14f6ca746da5de496931
kapil jhajhria <jhajhria44@gmail.com>
2021-01-17 12:59:17 +0530

    refactoring: static lookup func added to rental

--- routes/returns.js
+++ routes/returns.js
@@ -12,10 +12,7 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/", auth, validate(validateReturn), async (req, res) => {
-  const rental = await Rental.findOne({
-    "customer._id": req.body.customerId,
-    "movie._id": req.body.movieId,
-  });
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId);
 
   if (!rental) return res.status(404).send("Rental Not found");
 

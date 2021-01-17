9b06048359ed07224aad2bd8d8c395069142f68f
kapil jhajhria <jhajhria44@gmail.com>
2021-01-17 14:24:45 +0530

    helemt and compression module integrated for prod

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./startup/routes")(app); //load the routes
 require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
 require("./startup/validation")(); //Joi related statements req for api endpoint events validation
+require("./startup/prod")(app);
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)

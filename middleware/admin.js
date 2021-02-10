1343f5093f1f0fe46ffa9a6ccb1aee4a516e9a11
kapil jhajhria <jhajhria44@gmail.com>
2021-02-10 19:35:30 +0530

    added config to make auth optional

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,5 +1,9 @@
+const config = require("config");
+
 function admin(req, res, next) {
   const token = req.header("x-auth-token");
+
+  if (!config.get("requiresAuth")) return next();
 
   if (!req.user.isAdmin) {
     return res.status(403).send("Access denied. Unauthorized user");

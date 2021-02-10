2c59c3ab7d2a24d44c835a0242433680b120fb16
kapil jhajhria <jhajhria44@gmail.com>
2021-02-10 19:35:42 +0530

    added config to change port number

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("./startup/db")(); //connect to database
 require("./startup/config")(); //check for config values or env values. if not found throw error
 require("./startup/validation")(); //Joi related statements req for api endpoint events validation
 require("./startup/prod")(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
